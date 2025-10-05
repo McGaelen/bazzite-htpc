2081d8cceb4677024ae4b61fd0657a336f7d095c
McGaelen <mcinteeg@mail.gvsu.edu>
2025-10-04 21:29:19 -0400

    reorganize

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,16 +9,12 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# ========== keyd ==========
+dnf5 -y copr enable alternateved/keyd
+dnf5 -y install keyd
+dnf5 -y copr disable alternateved/keyd
+systemctl enable keyd
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# ========== ydotool ==========
+# comes pre-installed on bazzite
+systemctl enable ydotool
